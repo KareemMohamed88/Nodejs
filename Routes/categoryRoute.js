28b13bdcd45c2b8396ca4bedd80e4b469ca5a02b
Kareemm2 <132186526+KareemMohamed88@users.noreply.github.com>
2023-06-26 17:19:32 +0200

    test

--- Routes/categoryRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getCategory, createCategory } = require("../services/categoryServices");
-const router = express.Router();
-
-router.route("/").get(getCategory).post(createCategory);
-
-module.exports = router;
